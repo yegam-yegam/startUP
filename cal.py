c51e347c1df3d747f748139aff90c5d12e604a6c
yegam <kchan080716@gmail.com>
2025-07-15 22:39:08 +0900

    Update cal.py

--- cal.py
+++ cal.py
@@ -10,26 +10,22 @@ if st.button("계산하기"):
         l *= l
         r = round(w/l, 2)
         st.write(f"BMI: {r}")
-        print("\n    저체중            정상        과체중        비만                고도비만")
-        print("             18.5               23      25                  30")
-        print("              !                 !       !                   !                   ")
-        print("="*80)
-        print(" "*i,"^")
+        st.write("\n    저체중            정상        과체중        비만                고도비만")
+        st.write("             18.5               23      25                  30")
+        st.write("              !                 !       !                   !                   ")
+        st.write("="*80)
+        st.write(" "*i,"^")
 
         if bmi <= 18:
-            print("저체중 입니다.")
-        
+            st.write("저체중 입니다.")
         if bmi > 18 and bmi <= 23:
-            print("정상 입니다.")
-            
+            st.write("정상 입니다.")       
         if bmi > 23 and bmi <= 25:
-            print("과체중 입니다.")
-            
+            st.write("과체중 입니다.")       
         if bmi > 25 and bmi <= 30:
-            print("비만 입니다.")
-        
+            st.write("비만 입니다.")
         if bmi > 30:
-            print("고도비만 입니다.")
+            st.write("고도비만 입니다.")
     except Exception as e:
         st.error("오류 발생")
 
