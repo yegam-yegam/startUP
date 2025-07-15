76fbc36f787b8627f420f76be22bcc109fdf141e
yegam <kchan080716@gmail.com>
2025-07-15 22:32:43 +0900

    Update cal.py

--- cal.py
+++ cal.py
@@ -1,11 +1,16 @@
 import streamlit as st
 
 st.title("BMI 계산기")
-l = st.number_input("키(cm)", value=1)
-w = st.number_input("몸무게(kg)", value=1)
-l /= 100
-l *= l
-r = w/l
-st.write(f"BMI: {r}")
+l = st.number_input("키(cm)", value=0)
+w = st.number_input("몸무게(kg)", value=0)
+if st.button("계산하기"):
+    try:
+        l /= 100
+        l *= l
+        r = w/l
+        st.write(f"BMI: {r}")
+    except Exception as e:
+        st.error("오류 발생")
+
       
 
