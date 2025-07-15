8f355ee2354334a5493bbf041fbfb4379d4515d7
yegam <kchan080716@gmail.com>
2025-07-15 22:41:44 +0900

    Update cal.py

--- cal.py
+++ cal.py
@@ -6,14 +6,15 @@ w = st.number_input("몸무게(kg)", value=0)
 
 if st.button("계산하기"):
     try:
-        l /= 100
-        l *= l
-        r = round(w/l, 2)
+        r = round(w/((l/100)**2), 2)
         st.write(f"BMI: {r}")
         st.write("\n    저체중            정상        과체중        비만                고도비만")
         st.write("             18.5               23      25                  30")
         st.write("              !                 !       !                   !                   ")
         st.write("="*80)
+        i = round((r-15)*4, 0)
+        if i > 80:
+            i = 78
         st.write(" "*i,"^")
 
         if bmi <= 18:
