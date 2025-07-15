788c9c17124af611da5bdbd5f9a2498256678ec1
yegam <kchan080716@gmail.com>
2025-07-15 22:30:52 +0900

    Update cal.py

--- cal.py
+++ cal.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 st.title("BMI 계산기")
-l = st.number_input("키(cm)", value=0)
-w = st.number_input("몸무게(kg)", value=0)
+l = st.number_input("키(cm)", value=1)
+w = st.number_input("몸무게(kg)", value=1)
 l /= 100
 l *= l
 r = w/l
